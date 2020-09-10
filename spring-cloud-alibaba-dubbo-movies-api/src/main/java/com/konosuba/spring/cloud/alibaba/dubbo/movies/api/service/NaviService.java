b819f214b764874ec2098eb0927c5324bc216679
xiongtianle <2529814288@qq.com>
2020-09-10 17:21:36 +0800

    学到了一种更帅气的写法，以后要好好练好lambda表达式

--- spring-cloud-alibaba-dubbo-movies-api/src/main/java/com/konosuba/spring/cloud/alibaba/dubbo/movies/api/service/NaviService.java
+++ spring-cloud-alibaba-dubbo-movies-api/src/main/java/com/konosuba/spring/cloud/alibaba/dubbo/movies/api/service/NaviService.java
@@ -3,5 +3,5 @@ package com.konosuba.spring.cloud.alibaba.dubbo.movies.api.service;
 import com.konosuba.spring.cloud.alibaba.dubbo.movies.api.dto.NaviDto;
 
 public interface NaviService {
-    public NaviDto getNavi(int id);
+    public NaviDto getNavi(int id) throws Exception;
 }
--- spring-cloud-alibaba-dubbo-movies-films-service/movies-navi/src/main/java/com/konosuba/dubbo/movies/navi/service/impl/NaviServiceImpl.java
+++ spring-cloud-alibaba-dubbo-movies-films-service/movies-navi/src/main/java/com/konosuba/dubbo/movies/navi/service/impl/NaviServiceImpl.java
@@ -1,6 +1,8 @@
 package com.konosuba.dubbo.movies.navi.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.konosuba.dubbo.movies.navi.domain.entity.Navi;
 import com.konosuba.dubbo.movies.navi.mapper.NaviMapper;
 import com.konosuba.spring.cloud.alibaba.dubbo.movies.api.dto.NaviDto;
@@ -9,6 +11,8 @@ import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.BeanUtils;
 
 import javax.annotation.Resource;
+import java.nio.file.OpenOption;
+import java.util.Optional;
 
 @Service
 public class NaviServiceImpl implements NaviService {
@@ -17,12 +21,39 @@ public class NaviServiceImpl implements NaviService {
     NaviMapper naviMapper;
 
     @Override
-    public NaviDto getNavi(int id) {
-        QueryWrapper<Navi> qw = new QueryWrapper<>();
-        qw.eq(Navi.COL_NAVI_ID, id);
-        Navi navi = naviMapper.selectOne(qw);
+    public NaviDto getNavi(int id) throws Exception {
+//        QueryWrapper<Navi> qw = new QueryWrapper<>();
+//        qw.eq(Navi.COL_NAVI_ID, id);
+//        Navi navi = naviMapper.selectOne(qw);
+//        NaviDto naviDto = new NaviDto();
+//        BeanUtils.copyProperties(navi, naviDto);
+//        return naviDto;
+
+        Navi navi = Optional.ofNullable(
+                naviMapper.selectOne(
+                        Wrappers.lambdaQuery(Navi.class)
+                                .eq(Navi::getNaviId, id)
+                )
+        ).orElseThrow(()->new Exception("空"));
         NaviDto naviDto = new NaviDto();
         BeanUtils.copyProperties(navi, naviDto);
         return naviDto;
+
+
+
+//        @Override
+//        public String value(EssentialFactorValueParameter parameter) {
+//            return Optional.ofNullable(
+//                    materialItemAttributeRelDao.selectOne(
+//                            Wrappers.lambdaQuery(MaterialItemAttributeRelate.class)
+//                                    .eq(MaterialItemAttributeRelate::getMaterialAttributeId, parameter.getEssentialFactor().getMaterialAttributeId())
+//                                    .eq(MaterialItemAttributeRelate::getMaterialItemId, parameter.getMaterialItem().getMaterialId())
+//                    ))
+//                    .map(MaterialItemAttributeRelate::getAttributeValue)
+//                    .orElseThrow(() -> new BaseException(
+//                            "物料扩展属性获取失败 | 物料 [" + parameter.getMaterialItem().getMaterialCode()
+//                                    + "]，扩展属性 [" + parameter.getEssentialFactor().getMaterialAttributeName() + "]"));
+//        }
+
     }
 }
